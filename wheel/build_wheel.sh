de6a8689ff1d254538222dfcb74a3b85277735ec
Ethan Wee <158101733+ethanwee1@users.noreply.github.com>
2024-08-26 14:01:06 -0700

    Update build_wheel.sh

--- wheel/build_wheel.sh
+++ wheel/build_wheel.sh
@@ -109,7 +109,7 @@ fi
 build_version_modified=$(echo "$build_version" | sed 's/\(rocm6\.[0-9]*\.[0-9]*\)/\1+lw-/')
 echo build_version_modified
 # Create a consistent wheel package name to rename the wheel to
-wheel_filename_new="${TORCH_PACKAGE_NAME}-${build_version_modified}${build_number_prefix}-cp${python_nodot}-none-${mac_version}.whl"
+wheel_filename_new="${TORCH_PACKAGE_NAME}-${build_version_modified}${build_number_prefix}+lw-cp${python_nodot}-none-${mac_version}.whl"
 
 
 ###########################################################
